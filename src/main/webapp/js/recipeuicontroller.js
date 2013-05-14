ac9f6391a5934147da23554c981afbc358f4ed2e
Perttu Auramo <perttu.auramo@gmail.com>
2013-05-14 20:39:33 +0300

    More files

---
+++ src/main/webapp/js/recipeuicontroller.js
@@ -0,0 +1,22 @@
+var recipeUiController = (function() {
+    function initUi() {
+        recipeService.getRecipes(function(result) {
+            console.log(result)
+            if (_.isEmpty(result.recipes)) {
+                $('.new-recipe').show()
+                $('.recipe-list').hide()
+            } else {
+                $('.new-recipe').hide()
+                $('.recipe-list').show()
+                $('.recipe-list ul').html(getRecipeListHtml(result.recipes))
+            }
+        });
+        function getRecipeListHtml(recipes) {
+            return _(recipes).map(function(recipe) { return "<ul>" + recipe.name + "</ul>"; }).reduce(function (r1, r2) { return r1 + r2 })
+        }
+    }
+
+    return {
+        initUi: initUi
+    }
+})();
