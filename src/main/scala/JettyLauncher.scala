09656cc15d2d74dcaeda8a9ce711e68e76171ea4
Perttu Auramo <perttu.auramo@gmail.com>
2015-05-24 20:28:44 +0300

    change port

--- src/main/scala/JettyLauncher.scala
+++ src/main/scala/JettyLauncher.scala
@@ -1,11 +1,10 @@
-import org.eclipse.jetty.server.handler.HandlerCollection
 import org.eclipse.jetty.server.Server
-import org.eclipse.jetty.servlet.{ServletContextHandler, DefaultServlet}
+import org.eclipse.jetty.server.handler.HandlerCollection
 import org.eclipse.jetty.webapp.WebAppContext
 
 object JettyLauncher {
   def main(args: Array[String]) {
-    val port = if(System.getenv("PORT") != null) System.getenv("PORT").toInt else 8081
+    val port = if(System.getenv("PORT") != null) System.getenv("PORT").toInt else 8080
 
     val server = new Server(port)
     val handlerCollection = new HandlerCollection()
